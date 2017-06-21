57caf876bea4b1856d13623482c0e626d3de79f8
Warren Kretzschmar <wkretzsch@gmail.com>
2017-06-21 16:22:35 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Pydm',
-    version='0.1.0',
+    version='0.1.1',
     description='Python Distributed Make',
     author='Kiran Garimella',
     author_email='kiran.garimella@gmail.com',
