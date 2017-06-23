c5bcca6298f8c8647a3dc52e96f5b4bf48cd5714
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-06-23 11:49:01 +0100

    Rename arguments so their defaults are always False

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dmpy',
-    version='0.2.1',
+    version='0.3.0',
     description='Distributed Make for Python',
     author='Kiran Garimella',
     author_email='kiran.garimella@gmail.com',
