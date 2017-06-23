85415c153500d0706d831d5073488f2abe62d139
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-06-23 11:37:46 +0100

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dmpy',
-    version='0.2.0',
+    version='0.2.1',
     description='Distributed Make for Python',
     author='Kiran Garimella',
     author_email='kiran.garimella@gmail.com',
