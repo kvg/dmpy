4c1f1a1248b2bde09dcf9696c09799dd6ab0ac7c
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-06-23 15:46:42 +0100

    dry_run has been replaced by no_dry_run

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from dmpy.distributedmake import DistributedMake, get_dm_arg_parser
 
 # Pass --dry-run to command line
 args = get_dm_arg_parser().parse_args()
-m = DistributedMake(dry_run=False, keep_going=True, num_jobs=10, args_object=args)
+m = DistributedMake(keep_going=True, num_jobs=10, args_object=args)
 
 m.add("test_output_file", None, './test')
 m.execute()
