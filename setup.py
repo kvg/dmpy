6fba62b801648d9e3acc7c1f75fdd26603aed6cb
Warren Kretzschmar <wkretzsch@gmail.com>
2017-06-29 14:28:43 +0200

    Bump version for .DELETE_ON_ERROR

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dmpy',
-    version='0.3.2',
+    version='0.3.3',
     description='Distributed Make for Python',
     author='Kiran Garimella',
     author_email='kiran.garimella@gmail.com',
