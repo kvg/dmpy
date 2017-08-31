57ad51bd08c0c20a3f080872579abff657764832
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-08-31 12:31:13 +0200

    Bump version to 0.8.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dmpy',
-    version='0.8.2',
+    version='0.8.3',
     description='Distributed Make for Python',
     author='Kiran Garimella',
     author_email='kiran.garimella@gmail.com',
