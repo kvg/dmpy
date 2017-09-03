46d54bd66123821332015f377aad9b0436ddb2b2
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-09-03 22:26:11 +0200

    Fix missing package packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='dmpy',
-    version='0.10.0',
+    version='0.10.1',
     description='Distributed Make for Python',
     author='Kiran Garimella',
     author_email='kiran.garimella@gmail.com',
-    packages=['dmpy'],
+    packages=find_packages(),
 )
